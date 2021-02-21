d576db0de00ef23cd3a23acd80de190f30949d66
Massipssa <kerrache.massipssa@gmail.com>
2021-02-21 14:54:25 +0100

    Update docker_ci.sh

--- scripts/docker_ci.sh
+++ scripts/docker_ci.sh
@@ -3,4 +3,4 @@ USER="massipssa"
 REPO="devs"
 docker build -t devs/workspace .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "$USER"/"$REPO"
+docker push "$USER"/"$REPO":workspace
