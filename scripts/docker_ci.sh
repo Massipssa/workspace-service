762d3828572556d1847cc32cece18f6e304d8766
Massipssa <kerrache.massipssa@gmail.com>
2021-02-21 14:57:37 +0100

    Update docker_ci.sh

--- scripts/docker_ci.sh
+++ scripts/docker_ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+REGISTRY="hub.docker.com"
 USER="massipssa"
 REPO="devs"
 docker build -t devs/workspace .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$REGISTRY"
 docker push "$USER"/"$REPO":workspace
